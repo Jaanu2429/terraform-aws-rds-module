b3f88f14988dd48db3d1c3da9bd7cecc768f7f68
Jaanu2429 <150994702+Jaanu2429@users.noreply.github.com>
2024-06-26 12:32:17 +0530

    Update main.tf

---
+++ Terraform-aws-rds/main.tf
@@ -0,0 +1,81 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_subnet" "aurora_subnet" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  availability_zone = element(["us-west-2a", "us-west-2b"], count.index)
+
+  tags = {
+    Name = "aurora-subnet-${count.index}"
+  }
+}
+
+resource "aws_security_group" "aurora_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "aurora-sg"
+  }
+}
+
+resource "aws_rds_cluster" "aurora_cluster" {
+  cluster_identifier      = "aurora-cluster-demo"
+  engine                  = "aurora-postgresql"
+  engine_version          = "10.14"
+  master_username         = "auroraadmin"
+  master_password         = var.aurora_admin_password
+  db_subnet_group_name    = aws_db_subnet_group.aurora.id
+  vpc_security_group_ids  = [aws_security_group.aurora_sg.id]
+  skip_final_snapshot     = true
+
+  tags = {
+    Name = "aurora-cluster"
+  }
+}
+
+resource "aws_db_instance" "aurora_instance" {
+  count              = 2
+  identifier         = "aurora-instance-${count.index}"
+  cluster_identifier = aws_rds_cluster.aurora_cluster.id
+  instance_class     = "db.r5.large"
+  engine             = aws_rds_cluster.aurora_cluster.engine
+  engine_version     = aws_rds_cluster.aurora_cluster.engine_version
+
+  tags = {
+    Name = "aurora-instance-${count.index}"
+  }
+}
+
+resource "aws_db_subnet_group" "aurora" {
+  name       = "aurora-subnet-group"
+  subnet_ids = aws_subnet.aurora_subnet[*].id
+
+  tags = {
+    Name = "aurora-subnet-group"
+  }
+}
